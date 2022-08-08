fbd85af766b1a20cecd33bd0574294af356acc2a
pro-soichiro <dr.1987626@gmail.com>
2022-08-08 23:14:10 +0900

    ApolloClientを導入

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from "@apollo/client"
+
+const link = createHttpLink({
+  uri: "http://localhost:3000/graphql",
+  credentials: "include",
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
